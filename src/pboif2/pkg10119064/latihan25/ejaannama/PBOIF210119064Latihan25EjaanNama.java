7684fe34d3050a96e1445bd3c46a2cb4622120f5
Bagus_24 <Bagus_24@Bagus24>
2020-10-28 09:03:28 +0700

    new commit

---
+++ src/pboif2/pkg10119064/latihan25/ejaannama/PBOIF210119064Latihan25EjaanNama.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119064.latihan25.ejaannama;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Bagus_24
+ * NAMA         : Bagus Budi Wibowo
+ * KELAS        : PBOIF2 
+ * NIM          : 10119064 
+ * Deskripsi Program : Program ini berisi memecah nama menjadi sebuah ejaan 
+ */
+public class PBOIF210119064Latihan25EjaanNama {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void ejaNama(String nama){
+        System.out.println("Ejaan untuk " + "'" + nama + "'" + " adalah");
+        String kata[] = nama.split("|");
+        for (int i = 0; i < kata.length; i++) {
+            System.out.println("Huruf ke-" + (i+1) + " : " + kata[i]);
+        }
+    }
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama;
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.print("Masukkan nama depan anda untuk di eja : ");
+        nama = keyboard.next();
+        
+        ejaNama(nama);
+    
+    }
+    
+}
